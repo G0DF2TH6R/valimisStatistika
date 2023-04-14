09b0f7f6db00c70ff98896c5f36cc816bce83e1f
Joonas Meister <joonas838@gmail.com>
2023-04-14 11:34:51 +0300

    Parandasin kontrolle ja lukustasin akna suuruse muutmise

--- src/main/java/com/valimisstatistika/valimisstatistika2/Graafika.java
+++ src/main/java/com/valimisstatistika/valimisstatistika2/Graafika.java
@@ -6,13 +6,11 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -34,14 +32,14 @@ public class Graafika extends Application {
         Text tuhjus = new Text("");
         Text tuhjus1 = new Text("");
         Text tuhjus2 = new Text("");
-        Text error0 = new Text("");
-        TextField eesnimi = new TextField("Eesnimi");
-        Text error1 = new Text("");
-        TextField perenimi = new TextField("Perekonnanimi");
-        Text error2 = new Text("");
-        TextField isikukood = new TextField("Isikukood");
+        Text error0 = new Text("Eesnimi");
+        TextField eesnimi = new TextField("");
+        Text error1 = new Text("Perekonnanimi");
+        TextField perenimi = new TextField("");
+        Text error2 = new Text("Isikukood");
+        TextField isikukood = new TextField("");
         Button nupp = new Button("Logi sisse");
-        nupp.setPadding(new Insets(0,20,0,20));
+        nupp.setPadding(new Insets(0, 20, 0, 20));
         nupp.setStyle("-fx-background-color: #00adff; -fx-text-fill: white");
         nupp.setCursor(Cursor.HAND);
         vBox.setAlignment(Pos.BASELINE_CENTER);
@@ -51,9 +49,10 @@ public class Graafika extends Application {
             public void handle(ActionEvent event) {
                 System.out.println("logimiskatse");
                 if (!idKontroll(isikukood.getText())) {
-                    error2.setText("");
+                    error2.setFill(Color.BLACK);
+                    error2.setText("Isikukood");
                     System.out.println("Töötab");
-                     // Siit tuleks panna uus scene peale jms.
+                    // Siit tuleks panna uus scene peale jms.
 
 
                 } else {
@@ -61,27 +60,39 @@ public class Graafika extends Application {
                     error2.setText("*Vigane sisestus!");
                     error2.setFill(Color.RED);
                 }
-                error0.setText("");
-                error1.setText("");
+
                 String mittesobivad = "!@#$%&*()'+,-./:;<=>?[]^_`{|}1234567890";
-                for (int i = 0; i < eesnimi.getText().length(); i++) {
-                    char kontroll = eesnimi.getText().charAt(i);
-                    if (mittesobivad.contains(Character.toString(kontroll))) {
-                        error0.setText("*Vigane sisestus!");
-                        error0.setFill(Color.RED);
-                        break;
+                if (eesnimi.getText().length() >= 2) {
+                    error0.setText("Eesnimi");
+                    error0.setFill(Color.BLACK);
+                    for (int i = 0; i < eesnimi.getText().length(); i++) {
+                        char kontroll = eesnimi.getText().charAt(i);
+                        if (mittesobivad.contains(Character.toString(kontroll))) {
+                            error0.setText("*Vigane sisestus!");
+                            error0.setFill(Color.RED);
+                            break;
+                        }
                     }
+                } else {
+                    error0.setText("*Vigane sisestus!");
+                    error0.setFill(Color.RED);
                 }
 
-                for (int i = 0; i < perenimi.getText().length(); i++) {
-                    char kontroll = perenimi.getText().charAt(i);
-                    if (mittesobivad.contains(Character.toString(kontroll))) {
-                        error1.setText("*Vigane sisestus!");
-                        error1.setFill(Color.RED);
-                        break;
+                if (perenimi.getText().length() >= 2) {
+                    error1.setText("Perekonnanimi");
+                    error1.setFill(Color.BLACK);
+                    for (int i = 0; i <= perenimi.getText().length(); i++) {
+                        char kontroll = perenimi.getText().charAt(i);
+                        if (mittesobivad.contains(Character.toString(kontroll)) && perenimi.getText().length() < 2) {
+                            error1.setText("*Vigane sisestus!");
+                            error1.setFill(Color.RED);
+                            break;
+                        }
                     }
+                }else {
+                    error1.setText("*Vigane sisestus!");
+                    error1.setFill(Color.RED);
                 }
-
 
 
             }
@@ -100,9 +111,10 @@ public class Graafika extends Application {
         vBox.getChildren().add(tuhjus2);
 
         Scene login = new Scene(vBox, 500, 330);
-        
+
         primaryStage.setTitle("Login");
         primaryStage.setScene(login);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
@@ -121,9 +133,9 @@ public class Graafika extends Application {
 
         String[] osad = {isikuKood.substring(0, 1), isikuKood.substring(1, 3), isikuKood.substring(3, 5), isikuKood.substring(5, 7)};
 
-        if (!(3<Integer.parseInt(osad[0]) && Integer.parseInt(osad[0])<= 6))return true;
-        if (!(0<Integer.parseInt(osad[0]) && Integer.parseInt(osad[0])<= 12))return true;
-        if (!(0<Integer.parseInt(osad[0]) && Integer.parseInt(osad[0])<= 31))return true;
+        if (!(3 < Integer.parseInt(osad[0]) && Integer.parseInt(osad[0]) <= 6)) return true;
+        if (!(0 < Integer.parseInt(osad[0]) && Integer.parseInt(osad[0]) <= 12)) return true;
+        if (!(0 < Integer.parseInt(osad[0]) && Integer.parseInt(osad[0]) <= 31)) return true;
 
         return false;
     }
