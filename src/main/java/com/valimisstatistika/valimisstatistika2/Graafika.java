004c6df1df539fb45852965d4c30f2922d89e767
Joonas Meister <joonas838@gmail.com>
2023-04-28 11:03:27 +0300

    Parandasin kontrolle ja lukustasin akna suuruse muutmise

--- src/main/java/com/valimisstatistika/valimisstatistika2/Graafika.java
+++ src/main/java/com/valimisstatistika/valimisstatistika2/Graafika.java
@@ -7,16 +7,14 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class Graafika extends Application {
@@ -29,105 +27,131 @@ public class Graafika extends Application {
     public void start(Stage primaryStage) {
 
 
-        VBox vBox = new VBox();
-        vBox.setSpacing(10);
+        Label pealkiri = new Label("Tere tulemast E-valimiste keskkonda!");
+        pealkiri.setStyle("-fx-font-size: 24px; -fx-font-weight: bold");
+        Label selgitus = new Label("Siin saate hääletada mugavalt ja turvaliselt.\n" +
+                "Sisselogimiseks klõpsake allolevale nupule.");
 
-        Text pealkiri = new Text("Tere tulemast meie valimisteemalisse programmi!");
-        pealkiri.setStyle("-fx-font-size: 12pt");
-        Text tuhjus = new Text("");
-        Text tuhjus1 = new Text("");
-        Text tuhjus2 = new Text("");
-        Text error0 = new Text("Eesnimi");
-        TextField eesnimi = new TextField("");
-        Text error1 = new Text("Perekonnanimi");
-        TextField perenimi = new TextField("");
-        Text error2 = new Text("Isikukood");
-        TextField isikukood = new TextField("");
-        Button nupp = new Button("Logi sisse");
-        nupp.setPadding(new Insets(0, 20, 0, 20));
-        nupp.setStyle("-fx-background-color: #00adff; -fx-text-fill: white");
-        nupp.setCursor(Cursor.HAND);
-        vBox.setAlignment(Pos.BASELINE_CENTER);
 
-        nupp.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                boolean edukas0 = false;
-                boolean edukas1 = true;
-                boolean edukas2 = true;
+        Button edasiNupp = new Button("Alusta");
+        edasiNupp.setOnAction(event -> {
 
-                System.out.println("logimiskatse");
-                if (!idKontroll(isikukood.getText())) {
-                    edukas0 = true;
-                    error2.setFill(Color.BLACK);
-                    error2.setText("Isikukood");
-                } else {
+            VBox vBox = new VBox();
+            vBox.setSpacing(10);
 
-                    error2.setText("*Vigane sisestus!");
-                    error2.setFill(Color.RED);
-                }
+            Text pealkiri2 = new Text("Tere tulemast meie valimisteemalisse programmi!");
+            pealkiri.setStyle("-fx-font-size: 12pt");
+            Text tuhjus = new Text("");
+            Text tuhjus1 = new Text("");
+            Text tuhjus2 = new Text("");
+            Text error0 = new Text("Eesnimi");
+            TextField eesnimi = new TextField("ass");
+            Text error1 = new Text("Perekonnanimi");
+            TextField perenimi = new TextField("ass");
+            Text error2 = new Text("Isikukood");
+            TextField isikukood = new TextField("50111023847");
+            Button nupp = new Button("Logi sisse");
+            nupp.setPadding(new Insets(0, 20, 0, 20));
+            nupp.setStyle("-fx-background-color: #00adff; -fx-text-fill: white");
+            nupp.setCursor(Cursor.HAND);
+            vBox.setAlignment(Pos.BASELINE_CENTER);
 
-                String mittesobivad = "!@#$%&*()'+,-./:;<=>?[]^_`{|}1234567890";
-                if (eesnimi.getText().length() >= 2) {
-                    error0.setText("Eesnimi");
-                    error0.setFill(Color.BLACK);
-                    for (int i = 0; i < eesnimi.getText().length(); i++) {
-                        char kontroll = eesnimi.getText().charAt(i);
-                        if (mittesobivad.contains(Character.toString(kontroll))) {
-                            edukas1 = false;
-                            error0.setText("*Vigane sisestus!");
-                            error0.setFill(Color.RED);
-                            break;
-                        }
+            nupp.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    boolean edukas0 = false;
+                    boolean edukas1 = true;
+                    boolean edukas2 = true;
+
+                    System.out.println("logimiskatse");
+                    if (!idKontroll(isikukood.getText())) {
+                        edukas0 = true;
+                        error2.setFill(Color.BLACK);
+                        error2.setText("Isikukood");
+                    } else {
+
+                        error2.setText("*Vigane sisestus!");
+                        error2.setFill(Color.RED);
                     }
-                } else {
-                    edukas1 = false;
-                    error0.setText("*Vigane sisestus!");
-                    error0.setFill(Color.RED);
-                }
 
-                if (perenimi.getText().length() >= 2) {
-                    error1.setText("Perekonnanimi");
-                    error1.setFill(Color.BLACK);
-                    for (int i = 0; i < perenimi.getText().length(); i++) {
-                        char kontroll = perenimi.getText().charAt(i);
-                        if (mittesobivad.contains(Character.toString(kontroll)) && perenimi.getText().length() < 2) {
-                            edukas2 = false;
-                            error1.setText("*Vigane sisestus!");
-                            error1.setFill(Color.RED);
-                            break;
+                    String mittesobivad = "!@#$%&*()'+,-./:;<=>?[]^_`{|}1234567890";
+                    if (eesnimi.getText().length() >= 2) {
+                        error0.setText("Eesnimi");
+                        error0.setFill(Color.BLACK);
+                        for (int i = 0; i < eesnimi.getText().length(); i++) {
+                            char kontroll = eesnimi.getText().charAt(i);
+                            if (mittesobivad.contains(Character.toString(kontroll))) {
+                                edukas1 = false;
+                                error0.setText("*Vigane sisestus!");
+                                error0.setFill(Color.RED);
+                                break;
+                            }
                         }
+                    } else {
+                        edukas1 = false;
+                        error0.setText("*Vigane sisestus!");
+                        error0.setFill(Color.RED);
                     }
-                } else {
-                    edukas2 = false;
-                    error1.setText("*Vigane sisestus!");
-                    error1.setFill(Color.RED);
-                }
 
-                if (edukas0 && edukas1 && edukas2) {
-                    primaryStage.setScene(ValikG());
-                    primaryStage.setResizable(true);
+                    if (perenimi.getText().length() >= 2) {
+                        error1.setText("Perekonnanimi");
+                        error1.setFill(Color.BLACK);
+                        for (int i = 0; i < perenimi.getText().length(); i++) {
+                            char kontroll = perenimi.getText().charAt(i);
+                            if (mittesobivad.contains(Character.toString(kontroll)) && perenimi.getText().length() < 2) {
+                                edukas2 = false;
+                                error1.setText("*Vigane sisestus!");
+                                error1.setFill(Color.RED);
+                                break;
+                            }
+                        }
+                    } else {
+                        edukas2 = false;
+                        error1.setText("*Vigane sisestus!");
+                        error1.setFill(Color.RED);
+                    }
+
+                    if (edukas0 && edukas1 && edukas2) {
+                        primaryStage.setScene(ValikG());
+                        primaryStage.setResizable(true);
+                    }
                 }
-            }
+            });
+
+            vBox.getChildren().add(pealkiri);
+            vBox.getChildren().add(tuhjus);
+            vBox.getChildren().add(error0);
+            vBox.getChildren().add(eesnimi);
+            vBox.getChildren().add(error1);
+            vBox.getChildren().add(perenimi);
+            vBox.getChildren().add(error2);
+            vBox.getChildren().add(isikukood);
+            vBox.getChildren().add(tuhjus1);
+            vBox.getChildren().add(nupp);
+            vBox.getChildren().add(tuhjus2);
+
+            Scene login = new Scene(vBox, 500, 330);
+
+            primaryStage.setTitle("valimised");
+            primaryStage.setScene(login);
+            //primaryStage.setScene(Esileht());
+            primaryStage.setResizable(false);
         });
 
-        vBox.getChildren().add(pealkiri);
-        vBox.getChildren().add(tuhjus);
-        vBox.getChildren().add(error0);
-        vBox.getChildren().add(eesnimi);
-        vBox.getChildren().add(error1);
-        vBox.getChildren().add(perenimi);
-        vBox.getChildren().add(error2);
-        vBox.getChildren().add(isikukood);
-        vBox.getChildren().add(tuhjus1);
-        vBox.getChildren().add(nupp);
-        vBox.getChildren().add(tuhjus2);
+        // Paigutame elemendid keskmisse kasti
+        VBox keskKast = new VBox(10);
+        keskKast.setAlignment(Pos.CENTER);
+        keskKast.getChildren().addAll(pealkiri, selgitus, edasiNupp);
 
-        Scene login = new Scene(vBox, 500, 330);
+        // Loome esilehe layouti ja lisame keskmise kasti sinna keskele
+        BorderPane esileht = new BorderPane();
+        esileht.setCenter(keskKast);
+        esileht.setPadding(new Insets(20));
 
-        primaryStage.setTitle("Login");
-        primaryStage.setScene(login);
-        primaryStage.setResizable(false);
+        // Loome stseeni ja näitame seda
+        Scene stseen1 = new Scene(esileht, 600, 400, Color.LIGHTBLUE);
+
+        primaryStage.setScene(stseen1);
         primaryStage.show();
     }
 
@@ -177,5 +201,33 @@ public class Graafika extends Application {
         Scene valimine = new Scene(bp);
 
         return valimine;
+    }
+    public static Scene Esileht(){
+        BorderPane bp = new BorderPane();
+        Text tere = new Text("Tere tulemast e-valimis keskonda");
+        tere.setTextAlignment(TextAlignment.CENTER);
+        bp.setTop(tere);
+        Text seletus = new Text("On võimalik valida üks endale kõige sobivam erakond. " +
+                "Peale valiku tegemist on võimlik vaadata jooksvalt statistikat.");
+        seletus.wrappingWidthProperty();
+        bp.setCenter(seletus);
+
+        Button edasi = new Button("Edasi");
+        edasi.setPadding(new Insets(0, 20, 0, 20));
+        edasi.setStyle("-fx-background-color: #00adff; -fx-text-fill: white");
+        edasi.setCursor(Cursor.HAND);
+        edasi.setAlignment(Pos.BASELINE_CENTER);
+        bp.setBottom(edasi);
+
+        edasi.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        });
+
+
+        Scene esileht = new Scene(bp, 500,500);
+        return esileht;
     }
 }
